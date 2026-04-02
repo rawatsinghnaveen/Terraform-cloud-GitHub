099b5292c92e1b8fc0394b7c63b348bbb4edde98
rawatsinghnaveen <rawat.singh.naveen@gmail.com>
2026-04-02 08:52:36 +0530

    Update main.tf changed aws version by Naveen v2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 6.39.0"
      }
     }
    }
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "NAVEEN-INFRA" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+	ami = "ami-01b14b7ad41e17ba4" 
+	instance_type = "t3.micro"
 }
